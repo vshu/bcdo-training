d6fe2e0b1e311260ceb3cfb38f0aa942bf1cb57c
Vagrant <vagrant@example.com>
2013-10-18 23:32:54 +0000

    updated example-libc.py

--- example-libc.py
+++ example-libc.py
@@ -23,12 +23,18 @@ SECURITY_GROUP_NAMES = 'default'
 nodes = conn.list_nodes()
 sizes = conn.list_sizes()
 images = conn.list_images()
+locations = conn.list_locations()
 
 size = [s for s in sizes if s.id == 't1.micro'][0]
-image = images[0]
+image = images[1]
+image.id = "ami-acf9cde9"
+location = locations[1]
 
-pprint(nodes)
-pprint(size)
-pprint(image)
+#image="ami-acf9cde9"
 
-#node = conn.create_node(name='test-node-1', image=image, size=size, ex_keyname=KEYPAIR_NAME, ex_securitygroup=SECURITY_GROUP_NAMES)
+#pprint(nodes)
+#pprint(size)
+#pprint(image)
+#pprint(location)
+
+node = conn.create_node(name='test-node-1', image=image, size=size, location=location, ex_keyname=KEYPAIR_NAME, ex_securitygroup=SECURITY_GROUP_NAMES)
